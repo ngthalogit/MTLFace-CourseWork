20f152b20a667eda23a43a8237e875ee636a040c
ngthalogit <ngthlo.lap@MacBook-Pro-cua-Nguyen.local>
2022-06-09 14:20:40 +0700

    update save dir

--- models/mtlface.py
+++ models/mtlface.py
@@ -107,15 +107,6 @@ class MTLFace(object):
                 if  total_loss < loss_checkpoint:
                     loss_checkpoint = total_loss
                     self.fr.logger.checkpoints(n_iter)
-                # save loss
-                if n_iter == opt.num_iter + 1:
-                    loss_dict = {
-                        'id' : id_loss,
-                        'da' : da_loss,
-                        'age' : age_loss
-                    }
-                    df = pd.DataFrame.from_dict(loss_dict)
-                    df.to_csv('./loss.csv', index=False)
             if opt.train_fas:
                 # target_img, target_label
                 fas_inputs = self.fas.prefetcher.next()
@@ -127,7 +118,6 @@ class MTLFace(object):
 
             if n_iter % opt.val_interval == 0:
                 if opt.train_fr:
-                    pass
                     # loss = self.fr.validate(n_iter)
                     # id_loss.append(loss[0])
                     # da_loss.append(loss[1])
@@ -137,15 +127,15 @@ class MTLFace(object):
                     # if total_loss < loss_checkpoint:
                     #     loss_checkpoint = total_loss
                     #     self.fr.logger.checkpoints(n_iter)
-                    # # save loss to .csv
-                    # if n_iter == opt.num_iter + 1:
-                    #     loss_dict = {
-                    #         'id': id_loss,
-                    #         'da': da_loss,
-                    #         'age': age_loss
-                    #     }
-                    #     df = pd.DataFrame.from_dict(loss_dict)
-                    #     df.to_csv('/kaggle/working/MTLFace-CourseWork/loss.csv', index=False)
+
+                    # save loss to .csv
+                        loss_dict = {
+                            'id': id_loss,
+                            'da': da_loss,
+                            'age': age_loss
+                        }
+                        df = pd.DataFrame.from_dict(loss_dict)
+                        df.to_csv('./loss.csv', index=False)
                 if opt.train_fas:
                     pass
                     #self.fas.validate(n_iter)
